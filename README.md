e9a7f2f188010ee4b4b3373709e3c8fba0b05de5
jupo-ai <jupojupo.ai@gmail.com>
2025-03-18 15:29:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# YOLO-CENSOR
+
+## インストール
+1. 仮想環境作成、有効化
+   ```
+   python -m venv venv
+   ```
+   ```
+   .\venv\scripts\activate
+   ```
+
+2. cudaを使用する場合はtorch, torchvisionをインストール  
+   python3.12, cuda126でテストしていますが、他のバージョンでもだいたい動くと思います  
+   cpuでも動作しますが、だいぶ遅いです
+   ```
+   pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+   ```
+
+3. 他依存関係インストール
+   ```
+   pip install -r requirements.txt
+   ```
+
+
+## 使い方
+1. YOLOモデルのダウンロード  
+   civitai等から好きなモデルをダウンロードしてください
+
+2. config.tomlの編集  
+   
+   ![config](imgs/config.png)
+
+   - **general**
+     - device: cuda or cpu
+     - batch: バッチサイズ(ちょっと処理が早くなる)
+     - mode: 修正方法のモード(0: モザイク, 1: 黒埋め)
+   - **model**  
+     このmodelセクションは複数設定できます。
+     - path: モデルへのパス
+     - label: 検出するラベル(リストで複数指定可能)
+     - threshold: 検出する閾値
+     - mask: マスクのモード(segm: セグメント, bbox: ボックス)
+     - mask_offset: マスクをoffset分だけ拡大する
+
+3. main.pyの実行
+   ```
+   python main.py --target your/target/dir_or_file/path
+   ```
+   - **--target** 引数でフォルダかファイルを指定できます  
+   - **--target** 引数を省略した場合、CLIにてパスを質問されるので入力してください
+
+4. 画像と同じディレクトリにcensoredフォルダが作成され、修正が行われた場合のみ同名ファイルで保存されます。
+
+
+## 注意
+- 対象となるのは画像 (.png / .jpg / .jpeg / .webp) のみです。
+- **動画およびアニメーションwebpには対応していません (フォルダ内にあってもスキップされます)**
+- ComfyUI, sd-webui等のメタデータは削除されます。
+
+
+## TODO
+以下、時間があるときにやる
+- metadataの保持オプション
+- 動画、アニメーションwebp対応
+- 
---
+++ main.py
@@ -0,0 +1,416 @@
+from ultralytics import YOLO
+from pathlib import Path
+from tqdm import tqdm
+from dataclasses import dataclass, field
+from PIL import Image, ImageSequence
+from collections import defaultdict
+from typing import Union, Literal
+import questionary
+import torch
+import toml
+import cv2
+import math
+import numpy as np
+import argparse
+import imageio
+
+
+# ===============================================
+# コンフィグ
+# ===============================================
+@dataclass
+class GeneralSetting:
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    batch: int = 1
+    overwrite: bool = False
+    mode: int = 0 # 0: mosaic, 1: black fill
+    
+    def __post_init__(self):
+        if self.device == "cuda":
+            if not torch.cuda.is_available():
+                print("cuda使用不可のためcpuで処理します")
+                self.device = "cpu"
+
+@dataclass
+class ModelInfoSetting:
+    path: str = ""
+    label: list[str] = field(default_factory=list)
+    threshold: float = 0.5
+    mask: Literal["segm", "bbox"] = "bbox"
+    mask_offset: int = 4
+    
+    model: YOLO = field(init=False)
+    
+    def __post_init__(self):
+        if not isinstance(self.label, list):
+            self.label = [self.label]
+        
+        if self.path:
+            self.model = YOLO(self.path, verbose=False)
+
+@dataclass
+class RootConfig:
+    general: GeneralSetting
+    models: list[ModelInfoSetting]
+
+
+def load_config():
+    config = {}
+    with open("config.toml", mode="r", encoding="utf-8") as file:
+        config = toml.load(file)
+    
+    general = GeneralSetting(**config.get("general", {}))
+    model_list = config.get("model", [])
+    models = []
+    for info in model_list:
+        models.append(ModelInfoSetting(**info))
+    
+    return RootConfig(general=general, models=models)
+
+
+
+# ===============================================
+# ファイルの仕分け
+# 画像: png, jpg, jpeg, webp
+# 動画: mp4, webp
+# ===============================================
+def classification_pathes(files: list[Path]):
+    images = []
+    videos = []
+    for file in files:
+        if file.suffix == ".webp":
+            if webp_is_animated(file):
+                videos.append(file)
+            else:
+                images.append(file)
+        
+        elif file.suffix in [".png", ".jpg", ".jpeg"]:
+            images.append(file)
+        
+        elif file.suffix in [".mp4"]:
+            videos.append(file)
+    
+    return images, videos
+
+
+def webp_is_animated(file: Path):
+    try:
+        with Image.open(file) as img:
+            return img.is_animated
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+
+# ===============================================
+# バッチ作成
+# ===============================================
+def create_batches(files: list[Path], size):
+    batches = []
+    batch = []
+    for file in files:
+        batch.append(file)
+        if len(batch) == size:
+            batches.append(batch)
+            batch = []
+    
+    if batch:
+        batches.append(batch)
+    
+    return batches
+
+
+# ===============================================
+# 検閲処理
+# ===============================================
+def apply_mosaic(image: np.ndarray, region: np.ndarray) -> np.ndarray:
+    """
+    マスク領域にモザイク処理を適用する。
+    モザイクサイズはpixivに合わせて max(4, long/100) とする
+    """
+    height, width = image.shape[:2]
+    mosaic_size = math.ceil(max(4, max(height, width) / 100))
+    masked_image = image.copy()
+    mask_indices = region > 0
+    
+    # モザイク化
+    small_image = cv2.resize(
+        masked_image, 
+        (width // mosaic_size, height // mosaic_size), 
+        interpolation=cv2.INTER_NEAREST
+    )
+    mosaic_image = cv2.resize(
+        small_image, 
+        (width, height), 
+        interpolation=cv2.INTER_NEAREST
+    )
+    
+    # マスク領域にモザイクを適用
+    masked_image[mask_indices] = mosaic_image[mask_indices]
+    return masked_image
+
+
+def apply_black_fill(image: np.ndarray, region: np.ndarray) -> np.ndarray:
+    """
+    マスク領域を黒で塗りつぶす
+    """
+    masked_image = image.copy()
+    mask_indices = region > 0
+    masked_image[mask_indices] = [0, 0, 0]
+    return masked_image
+
+
+
+# ===============================================
+# メイン処理
+# ===============================================
+# 領域推論
+def predict_regions(input: Union[list[Path], np.ndarray], config: RootConfig):
+    """
+    各モデルで推論した領域を取得する
+    
+    Args:
+        input: 入力データ (画像パスリスト | 単一画像np.ndarray)
+        models: モデル情報のリスト (YOLOインスタンス, label, threshold)
+        config: 設定オブジェクト
+    
+    Returns:
+        regions: 検出された領域のリスト(np.ndarray) | dict(パスとnp.ndarrayリストのマップ)
+    """
+    should_mapping = isinstance(input, list)
+    regions = defaultdict(list) if should_mapping else []
+    if should_mapping: # 何も検出しなかった時ように明示的に初期化
+        for p in input:
+            regions[str(p)] = []
+
+    # 複数モデルで検出
+    for model_info in config.models:
+        results = model_info.model.predict(input, save=False, verbose=False, device=config.general.device)
+
+        for result in results:
+            boxes = result.boxes.xyxy
+            masks = result.masks
+            clses = result.boxes.cls
+            confs = result.boxes.conf
+            
+            for i, (box, cls, conf) in enumerate(zip(boxes, clses, confs)):
+                class_name = model_info.model.names[int(cls)]
+                if class_name in model_info.label and conf >= model_info.threshold:
+                    if masks is not None and i < len(masks) and model_info.mask == "segm":
+                        contour = masks.xy[i].astype(np.int32).reshape(-1, 1, 2)
+                        region = np.zeros(result.orig_shape, dtype=np.uint8)
+                        cv2.drawContours(region, [contour], -1, 255, cv2.FILLED)
+                    else:
+                        x_min, y_min, x_max, y_max = box.int().tolist()
+                        mask = np.zeros(result.orig_shape, dtype=np.uint8)
+                        mask[y_min:y_max, x_min:x_max] = 1
+                        region = mask
+                    
+                    # 領域をoffset分だけ拡張
+                    offset = int(model_info.mask_offset)
+                    if offset > 0:
+                        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2 * offset + 1, 2 * offset + 1))
+                        region = cv2.dilate(region, kernel, iterations=1)
+                else:
+                    region = None
+
+                if should_mapping:
+                    regions[result.path].append(region)
+                else:
+                    regions.append(region)
+    
+    return regions
+
+
+# 画像
+def process_images(images: list[Path], config: RootConfig):
+    print(f"画像処理を始めます: file数 {len(images)}")
+    
+    # バッチ作成
+    batches = create_batches(images, config.general.batch)
+    
+    bar = tqdm(total=len(images))
+    
+    for batch in batches:
+        regions_map = predict_regions(batch, config)
+        
+        # 後処理
+        for image_path, regions in regions_map.items():
+            bar.set_description(image_path)
+
+            pil_image = Image.open(image_path).convert("RGB")
+            image = np.array(pil_image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            should_save = False
+            for region in regions:
+                if region is not None:
+                    should_save = True
+                    
+                    if config.general.mode == 0:
+                        image = apply_mosaic(image, region)
+                    elif config.general.mode == 1:
+                        image = apply_black_fill(image, region)
+            
+            if should_save:
+                save_image(image, image_path)
+
+            bar.update(1)
+    
+    bar.close()
+
+
+def save_image(image: np.ndarray, orig_path: str):
+    output_dir = Path(orig_path).parent / "censored"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_path = output_dir / Path(orig_path).name
+
+    output_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    pil_output = Image.fromarray(output_image)
+    
+    pil_output.save(str(output_path))
+
+
+
+# 動画
+def process_videos(videos: list[Path], config: RootConfig):
+    print(f"動画処理を始めます: file数 {len(videos)}")
+
+    for file in videos:
+        # フレームジェネレータとその他情報を取得
+        frames, count, fps = get_video_frames(file)
+        if frames is None:
+            return
+        
+        bar = tqdm(total=count, desc=str(file))
+        processed_frames = []
+
+        should_save = False
+        
+        for frame in frames:
+            frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            regions = predict_regions(frame_bgr, config)
+            
+            for region in regions:
+                if region is not None:
+                    should_save = True
+                    
+                    if config.general.mode == 0:
+                        frame_bgr = apply_mosaic(frame_bgr, region)
+                    elif config.general.mode == 1:
+                        frame_bgr = apply_black_fill(frame_bgr, region)
+
+            processed_frame = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+            processed_frames.append(processed_frame)
+            bar.update(1)
+
+        if should_save or True:
+            save_video(processed_frames, file, fps, config)
+        bar.close()
+        
+
+def save_video(frames: list[np.ndarray], orig_path: Path, fps: float, config: RootConfig):
+    output_dir = Path(orig_path).parent / "censored"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    ouput_path = output_dir / Path(orig_path).name
+    
+    # webp
+    if orig_path.suffix.lower() == ".webp":
+        pil_frames = [Image.fromarray(frame) for frame in frames]
+        duration = int(1000 / fps)
+        imageio.mimwrite(ouput_path, pil_frames, format="webp", duration=duration, loop=0)
+    pass
+
+
+def get_video_frames(file: Path):
+    """
+    動画からフレーム画像をジェネレータとして取得する
+    """
+    if file.suffix.lower() == ".mp4":
+        cap = cv2.VideoCapture(str(file))
+        if not cap.isOpened():
+            print(f"動画を開けませんでした: {file}")
+            return None, None, None
+        
+        count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = cap.get(cv2.CAP_PROP_FPS)
+
+        def frame_generator():
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                yield cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            cap.release()
+        
+        return frame_generator(), count, fps
+    
+    elif file.suffix.lower() == ".webp":
+        img = Image.open(file)
+        if not img.is_animated:
+            print(f"アニメーションでないwebp: {file}")
+            return None, None, None
+        
+        count = img.n_frames
+        fps = 1000 / img.info.get("duration", 100) # durationはms単位、FPSに変換
+        
+        def frame_generator():
+            for frame in ImageSequence.Iterator(img):
+                yield np.array(frame.convert("RGB"))
+        
+        return frame_generator(), count, fps
+    
+    else:
+        print(f"サポートされていない形式: {file}")
+        return None, None, None
+                
+
+
+def main(args):
+    config = load_config()
+    
+    # 対象フォルダ / ファイル
+    target_path = args.target
+    if not target_path:
+        target_path = questionary.text("対象フォルダ / ファイル").ask().strip('"').strip()
+    if not target_path:
+        return
+    target_path = Path(target_path)
+    if not target_path.exists():
+        print("対象フォルダ/ファイルが存在しません")
+        return
+    
+    files = []
+    if target_path.is_file():
+        files = [target_path]
+    elif target_path.is_dir():
+        files = list(target_path.glob("*.*"))
+    
+    # ファイルを仕分ける
+    images, videos = classification_pathes(files)
+    
+    
+    # 画像を処理
+    if images:
+        process_images(images, config)
+    
+    # 動画を処理
+    if videos:
+        pass
+        # process_videos(videos, config)
+        
+
+
+
+    
+    
+    
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--target", type=str, default=None)
+    args, _ = parser.parse_known_args()
+    main(args)
